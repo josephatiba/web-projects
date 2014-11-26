7a6e3afb4350d74fbeffe65d8b421581c9ec8120
Joey Atiba <joey.atiba@gmail.com>
2014-11-26 10:28:32 -0800

    The inputs are lined up and the layout is basically in the right places. This needs to be styled but is functional.

--- js/main.js
+++ js/main.js
@@ -1,86 +1,84 @@
-
-var input_1 = document.getElementById("input_1");
-
-var input_2 = document.getElementById("input_2");
-
-var addition = document.getElementById("addition");
-
-var subraction = document.getElementById("subtraction");
-
-var multiplication = document.getElementById("multiplication");
-
-var division = document.getElementById("division");
-
-var clear = document.getElementById("clear");	
-
-var equals = document.getElementById("equals");
-
-var output = document.getElementById("output");
-
-var sum =  function () {
-	return Number(input_1.value) + Number(input_2.value);
-}
-
-var product =  function () {
-	return (Number(input_1.value) * Number(input_2.value));
-}
-
-var difference =  function () {
-	return (Number(input_1.value) - Number(input_2.value));
-}
-
-var quotient = function () {
-	return (Number(input_1.value) / Number(input_2.value));
-}
-
-var toZero = function () {
-	input_1.value = 0;
-	input_2.value = 0;
-	return output.value = 0;
-}
-
-//  var result = function () {
-// 	output.interText = eq
-// // }
-
-addition.onclick = function () {
-	console.log(sum());
-	output.innerText = sum();
-}
-
-subtraction.onclick = function () {
-	console.log(difference());
-	output.innerText = difference();
-}
-
-multiplication.onclick = function () {
-	console.log(product());
-	output.innerText = product();
-}
-
-division.onclick = function () {
-	console.log(quotient());
-	output.innerText = quotient();
-}
-
-clear.onclick = function () {
-	input_1.innerText = toZero();
-	input_2.innerText = toZero();
-	output.innerText = toZero();
-}
+(function() {
 
 
- equals.onclick = function () {
-	output.innerText = output();
-}
-	// switch (opr) {
-	// 	case "quotient":
-	// 	return 
-	// }
+			function Calculator(){
 
-// 	quotient.value = document.getElementById("result").innerHTML; 
 
-// }
+				var input_1 = document.getElementById("input_1");
+
+				var input_2 = document.getElementById("input_2");
+
+				var addition = document.getElementById("addition");
+
+				var subraction = document.getElementById("subtraction");
+
+				var multiplication = document.getElementById("multiplication");
+
+				var division = document.getElementById("division");
+
+				var clear = document.getElementById("clear");	
+
+				var equals = document.getElementById("equals");
+
+				var output = document.getElementById("output");
+
+				var sign = document.getElementById("sign");
+
+				var sum =  function () {
+					return Number(input_1.value) + Number(input_2.value);
+				};
+
+				var product =  function () {
+					return (Number(input_1.value) * Number(input_2.value));
+				};
+
+				var difference =  function () {
+					return (Number(input_1.value) - Number(input_2.value));
+				};
+
+				var quotient = function () {
+					return (Number(input_1.value) / Number(input_2.value));
+				};
+
+				var toZero = function (){
+					input_1.value = "";
+					input_2.value = "";
+					output.innerText = "";
+				};
+
+				addition.onclick = function () {
+					sign.innerText = addition.innerHTML;
+					output.innerText = sum();
+				};
+
+				subtraction.onclick = function () {
+					sign.innerText = subtraction.innerHTML;
+					output.innerText = difference();
+				};
+
+				multiplication.onclick = function () {
+					sign.innerText = multiplication.innerHTML;
+					output.innerText = product();
+				};
+
+				division.onclick = function () {
+					sign.innerText = division.innerHTML;
+					output.innerText = quotient();
+				};
+
+				clear.onclick = function () {
+					toZero();
+				};
+			}
+		
+
+		var CALCULATOR = new Calculator();	
+
+
+})();
+
+
+ 
 
 
 
